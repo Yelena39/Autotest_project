596ed66250aa41e750feadaff41e0a3b0c94ce23
Yelena39 <thiswebsite.master@gmail.com>
2022-11-10 19:02:12 +0200

    Added product_page and test_product_page files, updated base_page and locators files

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+	ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+	BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+	BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+	BOOK_NAME_ADDED = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
+	BOOK_PRICE_ADDED = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+class ProductPage(BasePage):
+
+    def add_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_CART_BUTTON), "The 'Add to basket' button is not present on the page"
+        add_to_cart_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_to_cart_btn.click()
+
+    def should_be_correct_product_name(self):
+        assert self.is_element_present(*ProductPageLocators.BOOK_NAME), "Book name is not present on the page"
+        assert self.is_element_present(*ProductPageLocators.BOOK_NAME_ADDED), "Book name is not present in the success message" 
+        product_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        product_name_added = self.browser.find_element(*ProductPageLocators.BOOK_NAME_ADDED).text
+        assert product_name in product_name_added, "The product name in the information message doesn't match the name of the product added to the cart"
+
+    def should_be_correct_product_price(self):
+        assert self.is_element_present(*ProductPageLocators.BOOK_PRICE), "Book price is not present on the page"
+        assert self.is_element_present(*ProductPageLocators.BOOK_PRICE_ADDED), "Book price is not present in the success message"
+        product_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        product_price_added = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_ADDED).text
+        assert product_price in product_price_added, "The product price in the information message doesn't match the price of the product added to the cart"
