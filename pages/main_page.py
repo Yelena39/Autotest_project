53de893807b5fdfa3e5d7288e15ee027487eca9e
Yelena39 <thiswebsite.master@gmail.com>
2022-11-20 12:49:51 +0200

    added documentation lines

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,11 @@ from .base_page import BasePage
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
+	"""Returns the BasePage object with the delegated arguments.
 	
+	:param args:
+    :param kwargs:
+
+    """
 	def __init__(self, *args, **kwargs):
 		super(MainPage, self).__init__(*args, **kwargs)
